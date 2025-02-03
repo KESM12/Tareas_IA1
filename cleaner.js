b79d3f7fca66b4600edb38aa3f129996e4c1acb4
Kevin Secaida <92343645+KESM12@users.noreply.github.com>
2025-02-03 08:30:48 -0600

    Update cleaner.js

--- cleaner.js
+++ cleaner.js
@@ -1,8 +1,10 @@
 // MIT License
 // Copyright (c) 2025 Kevin Secaida
 
+let contadorEstados = 0;
+
 function reflex_agent(location, state) {
-    if (state == "DIRTY") return "LIMPIO";
+    if (state == "SUCIO") return "LIMPIO";
     else if (location == "A") return "DERECHA";
     else if (location == "B") return "IZQUIERDA";
 }
@@ -11,7 +13,8 @@ function test(states) {
     var location = states[0];
     var state = states[0] == "A" ? states[1] : states[2];
     var action_result = reflex_agent(location, state);
-    document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | ACCIÓN: ").concat(action_result);
+    contadorEstados++;
+    document.getElementById("log").innerHTML += "<br>Estado #".concat(contadorEstados).concat(" | Location: ").concat(location).concat(" | ACCIÓN: ").concat(action_result);
 
     if (action_result == "LIMPIO") {
         if (location == "A") states[1] = "LIMPIO";
@@ -20,8 +23,8 @@ function test(states) {
     else if (action_result == "IZQUIERDA") states[0] = "A";
 
     // Verificar si ambos lugares están limpios
-    if (states[1] == "CLEAN" && states[2] == "LIMPIO") {
-        document.getElementById("log").innerHTML += "<br>Todo esta limpio. Deteniendo.....";
+    if (states[1] == "LIMPIO" && states[2] == "LIMPIO") {
+        document.getElementById("log").innerHTML += "<br>Todo está limpio. Deteniendo...";
         return;
     }
 
@@ -29,10 +32,10 @@ function test(states) {
     setTimeout(function() {
         if (Math.random() < 0.5) {
             states[1] = "SUCIO";
-            document.getElementById("log").innerHTML += "<br>La posición A esta sucia.";
+            document.getElementById("log").innerHTML += "<br>La posición A está sucia.";
         } else {
-            states[2] = "DIRTY";
-            document.getElementById("log").innerHTML += "<br>La posición B esta sucia.";
+            states[2] = "SUCIO";
+            document.getElementById("log").innerHTML += "<br>La posición B está sucia.";
         }
         setTimeout(function() { test(states); }, 1000);
     }, 1000);
